09789782ae344d2199896115a675eecb52ab5c8e
Pavel Skazhutin <paha.nexus@list.ru>
2025-02-08 16:16:24 +0900

    Replace sout with JUL

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,33 +5,38 @@ import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
+        logger.info("Таблица пользователей создана");
 
         userService.saveUser("John", "Doe", (byte) 25);
-        System.out.println("User с именем – John добавлен в базу данных");
+        logger.info("User с именем – John добавлен в базу данных");
 
         userService.saveUser("Jane", "Doe", (byte) 30);
-        System.out.println("User с именем – Jane добавлен в базу данных");
+        logger.info("User с именем – Jane добавлен в базу данных");
 
         userService.saveUser("Alice", "Smith", (byte) 22);
-        System.out.println("User с именем – Alice добавлен в базу данных");
+        logger.info("User с именем – Alice добавлен в базу данных");
 
         userService.saveUser("Bob", "Johnson", (byte) 35);
-        System.out.println("User с именем – Bob добавлен в базу данных");
-
+        logger.info("User с именем – Bob добавлен в базу данных");
 
         List<User> users = userService.getAllUsers();
         for (User user : users) {
-            System.out.println(user);
+            logger.info("Пользователь: " + user);
         }
 
         userService.cleanUsersTable();
+        logger.info("Таблица пользователей очищена");
 
         userService.dropUsersTable();
+        logger.info("Таблица пользователей удалена");
     }
 }
